6598a30cb6cb67abb0030d83f52c232a8a8c34a6
youness <younesserbai@gmail.com>
2024-12-26 21:15:39 +0100

    fix config

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import './aws-config';
 import '@aws-amplify/ui-react/styles.css';
-
+import './aws-config';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
