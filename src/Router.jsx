6a8bdc034fa7f9b8e2419dc9db7169d9e9c6dc6a
youness <younesserbai@gmail.com>
2024-12-26 21:40:44 +0100

    resolve ./pages/Auth/Login from src/Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"; // Import Routes and Route
 import Register from './pages/Auth/Register';
-import Login from './pages/Auth/Login';
+import Login from './pages/Auth/login.jsx';
 import VerifyOtp from './pages/Auth/Verify-otp';
 import ForgetPassword from "./pages/Auth/Forget-password";
 import ResetPassword from "./pages/Auth/Reset-password";
