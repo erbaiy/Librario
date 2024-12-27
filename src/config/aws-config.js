641b9937e0689694a8bfacfecbccb152c1acd4cf
youness <younesserbai@gmail.com>
2024-12-27 10:05:54 +0100

    Update User Pool ID and region configuration

--- src/config/aws-config.js
+++ src/config/aws-config.js
@@ -41,11 +41,11 @@ const config = {
   Auth: {
     Cognito: {
       //required
-      userPoolId: 'us-east-1_PVnm5MlEp',
-      userPoolClientId: '23f7ejt0s16je8297jbm7an9om',
+      userPoolId: 'eu-north-1_ha05GkcoW',
+      userPoolClientId: '11n23j689g5ahgb07vve9r9atj',
       
       // optional
-      region: 'us-east-1',
+      region: 'eu-north-1',
       signUpVerificationMethod: 'code',
       loginWith: {
         email: true,
@@ -71,3 +71,41 @@ const config = {
 Amplify.configure(config);
 
 export default config;
+
+// // @ts-ignore
+// import { Amplify } from 'aws-amplify';
+
+// const config = {
+//   Auth: {
+//     Cognito: {
+//       //required
+//       userPoolId: 'us-east-1_PVnm5MlEp',
+//       userPoolClientId: '23f7ejt0s16je8297jbm7an9om',
+      
+//       // optional
+//       region: 'us-east-1',
+//       signUpVerificationMethod: 'code',
+//       loginWith: {
+//         email: true,
+//         username: false,
+//         phone: false
+//       },
+//       userAttributes: {
+//         email: {
+//           required: true,
+//           // @ts-ignore
+//           mutable: true
+//         },
+//         name: {
+//           required: true,
+//           // @ts-ignore
+//           mutable: true
+//         }
+//       }
+//     }
+//   }
+// };
+
+// Amplify.configure(config);
+
+// export default config;
